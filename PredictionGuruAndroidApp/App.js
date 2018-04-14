9b1250e88d786f9100b53ddc03ec81888373455f
Md. Iftekher Islam <munna@virbenefits.com>
2018-04-14 22:52:21 +0600

    Usage of styling in mobile app.

--- PredictionGuruAndroidApp/App.js
+++ PredictionGuruAndroidApp/App.js
@@ -4,21 +4,56 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello IKA World!</Text>
+        <View style={styles.container}>
+            <View style={styles.topSection}>
+                <Text style={styles.statusBarTex}>IKA Status Bar!</Text>
+            </View>
+            <View style={styles.middleSection}>
+                <Text style={styles.blueBold}>Hello IKA World!!</Text>
+            </View>
+            <View style={styles.bottomSection}>
+                <Text style={styles.footerTex}>Buttons will appear here</Text>
+            </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-      borderStyle: 'solid',
-      borderColor: 'red',
-      borderWidth: 1
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        borderStyle: 'solid',
+        borderColor: 'red',
+        borderWidth: 1
+    },
+    blueBold: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30
+    },
+    topSection: {
+        backgroundColor: "steelblue",
+        height: 22,
+        alignItems: 'center'
+    },
+    middleSection: {
+        flex: 1,
+        backgroundColor: "powderblue",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    bottomSection: {
+        height: 50,
+        backgroundColor: "skyblue",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    statusBarTex: {
+        color: 'white',
+        fontWeight: 'bold'
+    },
+    footerTex: {
+        fontWeight: 'bold'
+    }
 });
